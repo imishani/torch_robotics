85436a575959a9be319be139dd7c76ccf5f03400
Itamar Mishani <imishani@gmail.com>
2024-03-14 13:54:35 -0400

    circ

--- torch_robotics/environments/env_circle_2d.py
+++ torch_robotics/environments/env_circle_2d.py
@@ -23,11 +23,11 @@ class EnvCircle2D(EnvBase):
         obj_list = [
             MultiSphereField(
                 np.array(
-                [[0, 0],
-                 ]),
+                    [[0, 0],
+                     ]),
                 np.array(
-                [0.3,
-                 ]
+                    [0.3,
+                     ]
                 )
                 ,
                 tensor_args=tensor_args
@@ -55,6 +55,130 @@ class EnvCircle2D(EnvBase):
             tensor_args=tensor_args,
             **kwargs
         )
+
+    def get_rrt_connect_params(self, robot=None):
+        params = dict(
+            n_iters=10000,
+            step_size=0.01,
+            n_radius=0.3,
+            n_pre_samples=50000,
+            max_time=50
+        )
+
+        if isinstance(robot, RobotPointMass):
+            return params
+        else:
+            raise NotImplementedError
+
+    def get_gpmp2_params(self, robot=None):
+        params = dict(
+            n_support_points=64,
+            n_interpolated_points=None,
+            dt=0.04,
+            opt_iters=300,
+            num_samples=64,
+            sigma_start=1e-5,
+            sigma_gp=1e-2,
+            sigma_goal_prior=1e-5,
+            sigma_coll=1e-5,
+            step_size=1e-1,
+            sigma_start_init=1e-4,
+            sigma_goal_init=1e-4,
+            sigma_gp_init=0.2,
+            sigma_start_sample=1e-4,
+            sigma_goal_sample=1e-4,
+            solver_params={
+                'delta': 1e-2,
+                'trust_region': True,
+                'method': 'cholesky',
+            },
+        )
+
+        if isinstance(robot, RobotPointMass):
+            return params
+        else:
+            raise NotImplementedError
+
+    def get_chomp_params(self, robot=None):
+        params = dict(
+            n_support_points=64,
+            dt=0.04,
+            opt_iters=1,  # Keep this 1 for visualization
+            weight_prior_cost=1e-4,
+            step_size=0.05,
+            grad_clip=0.05,
+            sigma_start_init=0.001,
+            sigma_goal_init=0.001,
+            sigma_gp_init=0.3,
+            pos_only=False,
+        )
+
+        if isinstance(robot, RobotPointMass):
+            return params
+        else:
+            raise NotImplementedError
+
+
+class EnvRandCircle2D(EnvBase):
+
+    def __init__(self,
+                 name='EnvRandCircle2D',
+                 tensor_args=None,
+                 precompute_sdf_obj_fixed=True,
+                 sdf_cell_size=0.005,
+                 circle_radius_range=(0.05, 0.3),
+                 n_circles=5,
+                 **kwargs
+                 ):
+        self.tensor_args = tensor_args
+        obj_list = self.generate_random_env(n_circles=n_circles, radius_range=circle_radius_range)
+
+        object_field = ObjectField(obj_list, 'randcirc2d')
+
+        super().__init__(
+            name=name,
+            limits=torch.tensor([[-1, -1], [1, 1]], **tensor_args),  # environments limits
+            obj_fixed_list=[object_field],
+            precompute_sdf_obj_fixed=precompute_sdf_obj_fixed,
+            sdf_cell_size=sdf_cell_size,
+            tensor_args=tensor_args,
+            **kwargs
+        )
+
+    def generate_random_env(self,
+                            n_circles=5,
+                            radius_range=(0.05, 0.3)):
+        obj_list = []
+        centers = []
+        radii = []
+
+        for _ in range(n_circles):
+            def sample():
+                return (torch.tensor([np.random.uniform(-1, 1),
+                                      np.random.uniform(-1, 1)],
+                                     **self.tensor_args).view(1, -1),
+                        torch.tensor(np.random.uniform(radius_range[0], radius_range[1]),
+                                     **self.tensor_args).view(1, -1))
+
+            center, radius = sample()
+            valid = False
+            while not valid:
+                valid = True
+                for c, r in zip(centers, radii):
+                    dist_temp = (torch.norm(center - c, dim=1) - r - radius).flatten()[0]
+                    if dist_temp < 0:
+                        valid = False
+                        center, radius = sample()
+                        break
+            centers.append(center)
+            radii.append(radius)
+
+        obj_list.append(MultiSphereField(
+            torch.cat(centers, dim=0),
+            torch.cat(radii, dim=0).flatten(),
+            tensor_args=self.tensor_args
+        ))
+        return obj_list
 
     def get_rrt_connect_params(self, robot=None):
         params = dict(
